74905fffca693a48be71dc156e54a2ef45301c8e
natemcc20 <natemcc30@gmail.com>
2025-07-14 00:20:32 -0700

    Fixed redirect loop

--- bungie.py
+++ bungie.py
@@ -1,33 +1,36 @@
-from flask import Flask, redirect, request, render_template
+from flask import Flask, redirect, request, render_template, session
 from requests_oauthlib import OAuth2Session
 import os
 
 app = Flask(__name__)
+app.secret_key = os.getenv("FLASK_SECRET_KEY", "super-secret-dev-key") 
 
 client_id = os.getenv('CLIENT_ID')
 client_secret = os.getenv('CLIENT_SECRET')
-redirect_uri = "https://game-ai-19pg.onrender.com" 
+redirect_uri = "https://game-ai-19pg.onrender.com/callback" 
 auth_base_url = "https://www.bungie.net/en/OAuth/Authorize"
 token_url = "https://www.bungie.net/platform/app/oauth/token/"
 
 @app.route("/")
 def index():
-    oauth = OAuth2Session(client_id, redirect_uri=redirect_uri)
-    auth_url, state = oauth.authorization_url(auth_base_url)
-    return redirect(auth_url)
+    if 'oauth_token' in session:
+        return render_template("game.html")
+    else:
+        oauth = OAuth2Session(client_id, redirect_uri=redirect_uri)
+        auth_url, state = oauth.authorization_url(auth_base_url)
+        session['oauth_state'] = state
+        return redirect(auth_url)
 
 @app.route("/callback")
 def callback():
-    oauth = OAuth2Session(client_id, redirect_uri=redirect_uri)
+    oauth = OAuth2Session(client_id, redirect_uri=redirect_uri, state=session.get('oauth_state'))
     token = oauth.fetch_token(
         token_url=token_url,
         client_secret=client_secret,
         authorization_response=request.url
     )
-    header = {"X-API-Key": os.getenv("BUNGIE_KEY")}
-    resp = oauth.get("https://www.bungie.net/Platform/Social/Friends", headers=header)
-
-    return render_template("game.html")
+    session['oauth_token'] = token  
+    return redirect("/")  
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
