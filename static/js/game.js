5fde5ed5f80629374f5cd5c85ab351e546048bcc
natemcc20 <natemcc30@gmail.com>
2025-07-15 12:53:32 -0700

    Added speech to text recognition

--- static/js/game.js
+++ static/js/game.js
@@ -1,12 +1,37 @@
 const mic = document.getElementById('mic');
 const glow = mic.querySelector('.glow');
 
+audioRecorder = new WebAudioRecorder(sourceNode, {
+  workerDir: "/static/js/"     
+});
+
 const showGlow = () => glow.classList.add('active');
 const hideGlow = () => glow.classList.remove('active'); 
 
-mic.addEventListener('mousedown', showGlow);
-mic.addEventListener('mouseup', hideGlow);
-mic.addEventListener('mouseleave', hideGlow);
+const recognition = new (window.SpeechRecognition ||
+  window.webkitSpeechRecognition ||
+  window.mozSpeechRecognition ||
+  window.msSpeechRecognition)();
+
+recognition.lang = LANG;
+
+recognition.onresult = (event) => {
+  const transcript = event.results[0][0].transcript;
+  outputDiv.textContent += ` ${transcript}`;
+};
+
+
+
+function onHold () {
+    mic.addEventListener('mousedown', showGlow);
+    mic.addEventListener('mouseup', hideGlow);
+    mic.addEventListener('mouseleave', hideGlow);
+    recognition.onstart = () => startButton.textContent = "Listening...";;
+    recognition.onend = () => startButton.textContent = "Start Voice Input";;
+    mic.addEventListener("mousedown", () => recognition.start());
+    mic.addEventListener("mouseup", () => recognition.stop()); 
+    console.log(transcript); 
+}
 
 mic.addEventListener('touchstart', showGlow);
 mic.addEventListener('touchend', hideGlow);
